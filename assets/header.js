dff355907e41b0337b775b1e5beeed97b88a4a73
Nigel Dorning <nigeldorning@gmail.com>
2023-01-22 16:10:02 +0000

    feat: Added header and product hero sections

---
+++ assets/header.js
@@ -0,0 +1,23 @@
+const headerTrigger = document.querySelector('.header__trigger');
+const headerOverlay = document.querySelector('.header__overlay');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+
+// Show mobile menu when trigger is clicked
+headerTrigger.addEventListener('click', () => {
+    mobileMenu.classList.remove('header__mobile-menu--hidden');
+    headerOverlay.style.display = 'block';
+});
+
+// Close mobile menu when overlay is clicked
+headerOverlay.addEventListener('click', () => {
+    mobileMenu.classList.add('header__mobile-menu--hidden');
+    headerOverlay.style.display = 'none';
+});
+
+// Close mobile menu when screen is greater than 1440px;
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1440) {
+        mobileMenu.classList.add('header__mobile-menu--hidden');
+        headerOverlay.style.display = 'none';
+    }
+});
